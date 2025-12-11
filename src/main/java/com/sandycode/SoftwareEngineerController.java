efb898c19acf59eb3a474e19ac80dcc0b6b4415c
santhosh <santhoshkmr.sdr@gmail.com>
2025-12-12 02:25:43 +0530

    Just a SprintBoot Project.

---
+++ src/main/java/com/sandycode/SoftwareEngineerController.java
@@ -0,0 +1,32 @@
+package com.sandycode;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController
+{
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers()
+    {
+        return softwareEngineerService.getAllSoftwareEngineer();
+    }
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id)
+    {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+    @PostMapping
+    public void addnewSoftwareEngineer( @RequestBody SoftwareEngineer softwareEngineer)
+    {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+}
