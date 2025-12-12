7779d5dea5d3918cd4d873a2a5772d764b2238d6
santhosh <santhoshkmr.sdr@gmail.com>
2025-12-13 00:47:11 +0530

    Added Post and Delete Mapping Ops

--- src/main/java/com/sandycode/SoftwareEngineerController.java
+++ src/main/java/com/sandycode/SoftwareEngineerController.java
@@ -13,20 +13,29 @@ public class SoftwareEngineerController
     public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
         this.softwareEngineerService = softwareEngineerService;
     }
-
-    @GetMapping
+    @GetMapping //Get All rows of Engineer
     public List<SoftwareEngineer> getEngineers()
     {
         return softwareEngineerService.getAllSoftwareEngineer();
     }
-    @GetMapping("{id}")
+    @GetMapping("{id}")// Get by ID
     public SoftwareEngineer getEngineerById(@PathVariable Integer id)
     {
         return softwareEngineerService.getSoftwareEngineerById(id);
     }
-    @PostMapping
+    @PostMapping// Post or insert a row
     public void addnewSoftwareEngineer( @RequestBody SoftwareEngineer softwareEngineer)
     {
         softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+    @PutMapping("{id}")// Put or Update a row by ID
+    public void updateEngineer(@PathVariable Integer id, @RequestBody SoftwareEngineer updatedEngineer)
+    {
+        softwareEngineerService.updateSoftwareEngineer(id, updatedEngineer);
+    }
+    @DeleteMapping("{id}")// Delete a row by ID
+    public void deleteEngineer(@PathVariable Integer id)
+    {
+        softwareEngineerService.deleteSoftwareEngineer(id);
     }
 }
