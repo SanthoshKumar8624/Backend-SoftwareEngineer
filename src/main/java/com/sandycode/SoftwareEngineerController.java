f342984cf3f24c04f3d27fbfef17552c711ea2e9
santhosh <santhoshkmr.sdr@gmail.com>
2025-12-17 14:17:42 +0530

    added React api port number

--- src/main/java/com/sandycode/SoftwareEngineerController.java
+++ src/main/java/com/sandycode/SoftwareEngineerController.java
@@ -3,6 +3,7 @@ package com.sandycode;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequestMapping("api/v1/software-engineers")
